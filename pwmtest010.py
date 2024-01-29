1eb0e909365cfb9ac515cd472af732eaf38eb2e7
VIVARND <149641928+VIVARND@users.noreply.github.com>
2024-01-29 16:06:25 +0900

    Update pwmtest010.py

--- pwmtest010.py
+++ pwmtest010.py
@@ -13,7 +13,8 @@ pwm_out.start(0) # 초기 듀티비 설정
 try:
     while True:
        pwm_value = GPIO.input(12) # 12번 핀의 PWM 신호를 읽음
-       print("PWM Value: ", '%.10f', pwm_value)
+
+       print("PWM Value: ", '%5.10f', %(pwm_value))
        pwm_out.ChangeDutyCycle(pwm_value) # 읽은 PWM 값을 13번 핀으로 출력
        time.sleep(0.1) #0.1초 동안 대기
 except KeyboardInterrupt:
