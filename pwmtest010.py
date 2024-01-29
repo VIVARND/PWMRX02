2bc0761a42db29d9c44b768701f7b61f031ed4ca
VIVARND <149641928+VIVARND@users.noreply.github.com>
2024-01-29 14:06:42 +0900

    Update pwmtest010.py

--- pwmtest010.py
+++ pwmtest010.py
@@ -11,7 +11,7 @@ pwm_out = GPIO.PWM(12, 100) # 12번 핀에 100Hz의 PWM 신호를 출력
 pwm_out.start(0) # 초기 듀티비 설정
 
 try:
-    While True:
+    while True:
     pwm_value = GPIO.input(18) # 18번 핀의 PWM 신호를 읽음
     print("PWM Value: ", pwm_value)
     pwm_out.ChangeDutyCycle(pwm_value) # 읽은 PWM 값을 12번 핀으로 출력
